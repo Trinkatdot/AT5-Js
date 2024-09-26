098a09926e247f32ea348a909c4f38e4884f026f
trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2024-09-26 17:31:27 -0300

    feat: fazendo o ex2

--- Ex2/script.js
+++ Ex2/script.js
@@ -1,1 +1,17 @@
-oi 
+/*nome completo
+numero de inscrição
+ano de nascimento   
+materias: natureza, humanas, linguagens, matematica, redação*/
+function resuktadovestibular(){
+    let alunos=[];
+    let notas= [];
+    let materias = [natureza, humanas, linguagens, matematica, redação];
+    
+    do{
+    infos = {};
+    let nome = prompt('informe seu nome completo:');
+    while (nome.trim() || nome.indexOf(' ') || )
+
+    }
+    
+}
